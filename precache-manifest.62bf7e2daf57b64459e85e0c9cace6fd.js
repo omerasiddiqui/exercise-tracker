02cf47a06510aa452882be127d16a8fc6b0c0a8d
Omer Siddiqui <omersiddiqui@Omers-MacBook-2.local>
2020-02-09 16:53:24 -0700

    Updates

---
+++ precache-manifest.62bf7e2daf57b64459e85e0c9cace6fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d8a95d9d3609f77ffd41cbb6c99fc5",
+    "url": "/exercise-tracker/index.html"
+  },
+  {
+    "revision": "4da7b3204195dd2d1be4",
+    "url": "/exercise-tracker/static/css/2.46b9f2a8.chunk.css"
+  },
+  {
+    "revision": "f244bca4b533e8d1fbd1",
+    "url": "/exercise-tracker/static/css/main.d21f2ddc.chunk.css"
+  },
+  {
+    "revision": "4da7b3204195dd2d1be4",
+    "url": "/exercise-tracker/static/js/2.97ce89aa.chunk.js"
+  },
+  {
+    "revision": "4c3b865ea8d319faf1c56c47d6e1060e",
+    "url": "/exercise-tracker/static/js/2.97ce89aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f244bca4b533e8d1fbd1",
+    "url": "/exercise-tracker/static/js/main.03993d08.chunk.js"
+  },
+  {
+    "revision": "76dfcf1c925ea924e974",
+    "url": "/exercise-tracker/static/js/runtime-main.6a1da7c1.js"
+  }
+]);
